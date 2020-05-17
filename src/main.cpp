ada0010aa448a1b4d5ce58d9611b50da4408b1ac
Haustuer <haustuer@live.de>
2020-05-17 19:15:16 +0200

    GPGLL

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,9 @@
 #include <LoRa.h>
 #define myID 2
 //define the pins used by the transceiver module
-#define ss 18//5
-#define rst 14
-#define dio0 2
+#define ss_pin 18//5
+#define rst_pin 14
+#define dio0_pin 2
 #define ADC_1 36 
 #define ledpin 25 
 int counter = 0;
@@ -33,7 +33,7 @@ Serial2.begin(9600);
   while (!Serial);
   Serial.println("LoRa Repeater");
   //setup LoRa transceiver module
-  LoRa.setPins(ss, rst, dio0);  
+  LoRa.setPins(ss_pin, rst_pin, dio0_pin);  
   //replace the LoRa.begin(---E-) argument with your location's frequency 
   //433E6 for Asia
   //866E6 for Europe
@@ -55,16 +55,158 @@ Serial2.begin(9600);
   pinMode(ADC_1,INPUT);
 }
 
+// for Serial
 int incomingByte = 0; // for incoming serial data
+String inputString="";
+String dataString="";
+bool stringComplete=false;
+unsigned int dataPos=0;
+unsigned int dataPosOld=0; 
+String GPS_word="";/*
+  GLL - Geographic Position - Latitude/Longitude 
+           1       2 3        4 5         6 7 
+       |       | |        | |         | | 
+$--GLL,llll.ll,a,yyyyy.yy,a,hhmmss.ss,A*hh<CR><LF> 
+ Field Number:  
+  1) Latitude 
+  2) N or S (North or South) 
+  3) Longitude 
+  4) E or W (East or West) 
+  5) Universal Time Coordinated (UTC) 
+  6) Status A - Data Valid, V - Data Invalid , P - Precise
+  7) Checksum */
+String lat;
+String lon;
+String lat_;
+String lon_;
+String UTC;
+String sHH,sMM,sSS;
+int hh;
+int mm;
+int ss;
+
+
 int sensIN;
 void loop() {
  
  while (Serial2.available()) {
     // get the new byte:
     char inChar = (char)Serial2.read();
-     Serial.print(inChar);
-    //Serial.println("2");
- }
+    // add it to the inputString:
+    
+        // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '$') {
+      dataString="";
+      inputString="";
+      dataPosOld=dataPos;
+      dataPos=0;
+      stringComplete = true;
+    }  else {
+      inputString += inChar;
+      }  
+    if (inChar == ',') {  
+      //Serial.print(dataPos);
+      //Serial.print(":");
+     // Serial.println(dataString);
+
+      
+      if (dataPos==0){
+        GPS_word=dataString;
+      }else{
+        if (GPS_word=="$GPGLL"){
+        switch (dataPos)
+        {
+        case 1:
+          /* code */
+          lat=dataString;
+
+          break;
+        case 2:
+         lat_=dataString;
+
+          /* code */
+          break;
+           case 3:
+           
+lon=dataString;
+          /* code */
+          break;
+           case 4:
+           lon_=dataString;
+
+          /* code */
+          break;
+           case 5:
+           UTC=dataString;
+          // String str=UTC.substring(0,2);
+          sHH=UTC.substring(0,2);
+          hh=sHH.toInt();
+          sMM=UTC.substring(2,4);
+          mm=sMM.toInt();
+          sSS=UTC.substring(4,6);
+          ss=sSS.toInt();
+          /* code */
+          break;
+        //default:
+         // break;
+        }
+
+
+
+    }
+
+
+      }
+
+
+       dataString="";
+       dataPos++;
+    }else{
+      dataString+= inChar;
+    }
+    
+    
+
+
+  }
+
+  /*
+  GLL - Geographic Position - Latitude/Longitude 
+           1       2 3        4 5         6 7 
+       |       | |        | |         | | 
+$--GLL,llll.ll,a,yyyyy.yy,a,hhmmss.ss,A*hh<CR><LF> 
+ Field Number:  
+  1) Latitude 
+  2) N or S (North or South) 
+  3) Longitude 
+  4) E or W (East or West) 
+  5) Universal Time Coordinated (UTC) 
+  6) Status A - Data Valid, V - Data Invalid , P - Precise
+  7) Checksum */
+
+// $GPGGA,HHMMSS.ss,BBBB.BBBB,b,LLLLL.LLLL,l,Q,NN,D.D,H.H,h,G.G,g,A.A,RRRR*PP
+
+
+  if (stringComplete){
+     stringComplete = false;
+     
+    // Serial.println(inputString);
+       if (GPS_word=="$GPGLL"){
+          String Stamp=lat;
+                  Stamp+=lat_;
+                  Stamp+=";";
+                  Stamp+=lon;
+                  Stamp+=lon_;
+                  Stamp+=";";
+                   Stamp+=sHH;
+                    Stamp+=":";
+                     Stamp+=sMM;
+                     Stamp+=":";
+                     Stamp+=sSS;
+  Serial.println(Stamp);
+       }
+  }    
 
 
 
